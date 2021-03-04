73f546b5d045c49a136fcabad59a8bf9476d1765
eliapetrosino <79785064+eliapetrosino@users.noreply.github.com>
2021-03-04 22:11:44 +0100

    Update EmptyClass.cs

--- EmptyClass.cs
+++ EmptyClass.cs
@@ -1,21 +1,19 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-
 namespace MyUtilities.ClassGenerator
 {
    public class EmptyClass
    {
-      public string Name { get; set; }
-      public string Scope { get; set; }
-      public string Behave { get; set; }
+      public string Name { get; set; } = "";
+      public string Scope { get; set; } = "";
+      public string Behave { get; set; } = "";
       public ICollection<string> Properties { get; set; }
       public ICollection<string> Fields { get; set; }
+      private List<string> Types = new List<string>
+      { "string", "bool", "int", "uint", "long", "short", "decimal", "float", "double", "byte", "sbyte" };
 
 
-      private IEnumerable<string> Shape {
-         get => new List<string>
+      public IEnumerable<string> Shape()
+      {
+         return new List<string>
          {
             "using System;", "using System.Linq;",
             "using System.Collections.Generic;",
@@ -29,24 +27,39 @@ namespace MyUtilities.ClassGenerator
          };
       }
 
-      public string SplitFields() {
+      public void CreateFields()
+      {
+         Fields = new List<string>();
+         foreach (var p in Properties)
+            Fields.Add(p.ToLower());
+      }
+
+      protected string SplitFields()
+      {
          if (Fields == null) return "";
          var builder = new StringBuilder();
-         foreach (var f in Fields) builder.Append(f.Trim() + ";\n");
+         foreach (var f in Fields)
+            builder.Append(f.Trim() + ";\n");
          return builder.ToString();
       }
 
-      public string SplitProperties() {
+      protected string SplitProperties()
+      {
          if (Properties.First() == "") return "";
          var builder = new StringBuilder();
-         foreach (var p in Properties)
-         builder.Append("public " + p + " { get; set; }" + "\n");
+         bool propertyIsRightTyped = false;
+
+         foreach (var p in Properties) {
+            Types.ForEach(t => { if (p.Contains(t)) propertyIsRightTyped = true; });
+            builder.Append($"public {(propertyIsRightTyped ? p : "string " + p)} " + "{ get; set; }\n");
+         }
          return builder.ToString();
       }
 
-      public string SaveAsDotCS(string filename) {
-         File.WriteAllLines($@".\{filename}.cs", Shape);
-         return $"Class saved to {filename}.cs";
+      public string SaveAsDotCS(string path)
+      {
+         File.WriteAllLines($@"{path}\{Name}.cs", Shape());
+         return $"Class saved to {Name}.cs into {path}";
       }
    }
 }
